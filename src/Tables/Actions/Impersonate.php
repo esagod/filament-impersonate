5d31901af9b1354644457968fdbb5a46a434621e
Davide Pilato <de.pilato@gmail.com>
2025-05-06 02:34:21 +0200

    fix multi tenancy impersonate

--- src/Tables/Actions/Impersonate.php
+++ src/Tables/Actions/Impersonate.php
@@ -1,23 +1,44 @@
 <?php
-
 namespace Esagod\FilamentImpersonate\Tables\Actions;
 
-use Filament\Tables\Actions\Action;
+use Closure;
 use Esagod\FilamentImpersonate\Concerns\Impersonates;
+use Filament\Tables\Actions\Action;
+use Illuminate\Database\Eloquent\Model;
 
 class Impersonate extends Action
 {
-    use Impersonates;
+	use Impersonates;
+	protected Model | Closure | null $_record = null;
+	protected function setUp(): void
+	{
+		parent::setUp();
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this
-            ->label(__('filament-impersonate::action.label'))
-            ->iconButton()
-            ->icon('impersonate-icon')
-            ->action(fn ($record) => $this->impersonate($record))
-            ->hidden(fn ($record) => !$this->canBeImpersonated($record));
-    }
+		$this
+			->label(__('filament-impersonate::action.label'))
+			->iconButton()
+			->icon('impersonate-icon')
+			->action(function($record) {
+				if ($record->impersonate) {
+					if(is_a($record->impersonate, 'Illuminate\Database\Eloquent\Collection')) {
+						return $this->impersonate($record->impersonate[0]);
+					} else {
+						return $this->impersonate($record->impersonate);
+					}
+				} else {
+					return $this->impersonate($record);
+				}
+			})
+			->hidden(function($record) {
+				if ($record->impersonate) {
+					if(is_a($record->impersonate, 'Illuminate\Database\Eloquent\Collection')) {
+						return !$this->canBeImpersonated($record->impersonate[0]);
+					} else {
+						return !$this->canBeImpersonated($record->impersonate);
+					}
+				} else {
+					return !$this->canBeImpersonated($record);
+				}
+			});
+	}
 }
